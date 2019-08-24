956ea1b1b15f0b1f4a20b09b237b8e1109774633
Panela Leung <leungpanela@gmail.com>
2019-08-24 12:49:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ def line (array)
   if array.length == 0
   puts "The line is currently empty."
   elsif array.length >=0
-    number = array.each.with_index do |person, index +1|
+    number = array.each.with_index do |person, index|
     puts "The line is currently #{index}. #{person}"
+    end
   end
 end
 
