0e3a9ee06a4622a1a9e1a283c70005041b49cbde
Panela Leung <leungpanela@gmail.com>
2019-08-24 13:15:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line (array)
   #  puts "The line is currently
     #(array.each_with_index do |person, index|)
      #{index +1}. #{person}"
-  puts "The line is currently"
-    array.each_with_index do |person, index|
-    print  "#{index +1}. #{person}"
+  puts "The line is currently
+    (array.each_with_index do |person, index|
+   #{index +1}. #{person}"
 
     end
   end
