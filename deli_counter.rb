24bca93e420b4d0e86a1edc9040c409eddec7691
Panela Leung <leungpanela@gmail.com>
2019-08-24 12:36:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line (array)
+  #katz_deli = []
+  if array.length == 0
+  puts "The line is currently empty."
+  elsif array.length >=0
+  
+  end
+end
+
+def take_a_number(array,name)
+
+end
+
+def now_serving(array)
+  line(array)
+end
